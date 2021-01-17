6ce6f9fe2bb10abef76c77bc6708f297f7d8063c
Terribleangel <rundowncrew@hotmail.com>
2021-01-16 23:20:13 -0500

    Update testscript1.js

--- testscript1.js
+++ testscript1.js
@@ -1,7 +1,9 @@
+window.onload = function(){
 document.getElementById('script-tag-app').onclick = test();
 
 function test() {
-    alert('clicked on my script snippet');
+    console.log('clicked on my script snippet');
 }
 
 console.log('test script loaded');
+}
