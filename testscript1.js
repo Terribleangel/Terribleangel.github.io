1e34e0ef88335cb1fcdc8929c35fc937ea89f7a9
Terribleangel <rundowncrew@hotmail.com>
2021-01-16 23:20:40 -0500

    Update testscript1.js

--- testscript1.js
+++ testscript1.js
@@ -2,7 +2,7 @@ window.onload = function(){
 document.getElementById('script-tag-app').onclick = test();
 
 function test() {
-    console.log('clicked on my script snippet');
+    console.log('clicked on my test snippet');
 }
 
 console.log('test script loaded');
